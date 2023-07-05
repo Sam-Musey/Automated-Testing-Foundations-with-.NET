e154583b492d494574dcbe56f365ebef7ca0a187
msv <msvyatoslav90@gmail.com>
2023-07-05 20:42:10 +0500

    Added some validations to Vehicle class

--- Car park/Program.cs
+++ Car park/Program.cs
@@ -15,12 +15,12 @@ namespace Program
             // --- Creating two objects of passenger car and printing their description ---
 
             PassengerCar vwPolo = new PassengerCar(
-                "Volkswagen Polo", new Engine(207, 1984, "L4", 892384797), new Chassis(4, 514, 666777), new Transmission(7, "VW", "automatic", 0012983));
+                "Volkswagen Polo", new Engine(207, 1984, "L4", 892384797), new Chassis(4, 514, 66667777), new Transmission(7, "VW", "automatic", 1012983));
             Console.WriteLine(" --- Here are two passenger cars and their description: ---\n");
             vwPolo.Describe();
 
             PassengerCar jiguli = new PassengerCar(
-                "Jiguli", new Engine(78, 1568, "L4", 892387834), new Chassis(4, 400, 0012446), new Transmission(4, "AvtoVAZ", "manual", 666789));
+                "Jiguli", new Engine(78, 1568, "L4", 892387834), new Chassis(4, 400, 10012446), new Transmission(4, "AvtoVAZ", "manual", 6663789));
             Console.WriteLine("\n");
             jiguli.Describe();
 
@@ -28,18 +28,18 @@ namespace Program
             // --- Creating three objects of truck and printing their description ---
 
             Truck fordTruck = new Truck(
-                "Ford F-150", new Engine(400, 4950, "V8", 454794857), new Chassis(4, 970, 123724396), new Transmission(10, "Ford", "automatic", 777888));
+                "Ford F-150", new Engine(400, 4950, "V8", 454794857), new Chassis(4, 970, 12372439), new Transmission(10, "Ford", "automatic", 7772888));
             Console.WriteLine("\n");
             Console.WriteLine(" --- Here are three trucks and their description: ---\n");
             fordTruck.Describe();
 
             Truck toyotaTruck = new Truck(
-                "Toyota Tacoma", new Engine(159, 2700, "L4", 454794859), new Chassis(4, 700, 123724282), new Transmission(6, "Toyota", "manual", 777111));
+                "Toyota Tacoma", new Engine(159, 2700, "L4", 454794859), new Chassis(4, 700, 12372428), new Transmission(6, "Toyota", "manual", 7774111));
             Console.WriteLine("\n");
             toyotaTruck.Describe();
 
             Truck gmcTruck = new Truck(
-                "GMC Sierra 2500HD", new Engine(401, 6600, "V8", 454784859), new Chassis(4, 1620, 120124282), new Transmission(6, "GMC", "automatic", 777221));
+                "GMC Sierra 2500HD", new Engine(401, 6600, "V8", 454784859), new Chassis(4, 1620, 12012428), new Transmission(6, "GMC", "automatic", 7775221));
             Console.WriteLine("\n");
             gmcTruck.Describe();
 
@@ -47,20 +47,20 @@ namespace Program
             // --- Creating two objects of bus and printing their description ---
 
             Bus volvoBus = new Bus(
-                "Volvo 9700", new Engine(435, 13000, "D13", 882802304), new Chassis(8, 8000, 195376294), new Transmission(12, "Volvo", "automatic", 888999), 56);
+                "Volvo 9700", new Engine(435, 13000, "D13", 882802304), new Chassis(8, 8000, 19537629), new Transmission(12, "Volvo", "automatic", 8886999), 56);
             Console.WriteLine("\n");
             Console.WriteLine(" --- Here are two buses and their description: ---\n");
             volvoBus.Describe();
 
             Bus mercedesBus = new Bus(
-                "Mercedes-Benz Citaro", new Engine(300, 7700, "L6", 882810304), new Chassis(8, 5600, 195946294), new Transmission(4, "Mercedes-Benz", "automatic", 884599), 28);
+                "Mercedes-Benz Citaro", new Engine(300, 7700, "L6", 882810304), new Chassis(8, 5600, 19594629), new Transmission(4, "Mercedes-Benz", "automatic", 8847599), 28);
             Console.WriteLine("\n");
             mercedesBus.Describe();
 
             // --- Creating an object of scotter and printing its description ---
 
             Scooter hondaScooter = new Scooter(
-                "Honda Rukus", new Engine(4.4, 49, "single-cylinder four-stroke", 777234903), new Chassis(2, 0, 1992887234), new Transmission(2, "Honda", "automatic", 999000));
+                "Honda Rukus", new Engine(4.4, 49, "single-cylinder four-stroke", 777234903), new Chassis(2, 0, 19928873), new Transmission(2, "Honda", "automatic", 9991000));
             Console.WriteLine("\n");
             Console.WriteLine(" --- Here is a scooter and its description: ---\n");
             hondaScooter.Describe();
--- Car park/Vehicles/Vehicle.cs
+++ Car park/Vehicles/Vehicle.cs
@@ -15,14 +15,40 @@ namespace CarPark
 
         public double EnginePower
         {
-            get { return Engine.Power; }
-            set { Engine.Power = value; }
+            get
+            {
+                if (Engine.Power > 0)
+                    return Engine.Power;
+                else
+                    throw new Exception("The power of an engine can not be negative or equal to 0.");
+            }
+
+            set
+            {
+                if (Engine.Power > 0)
+                    Engine.Power = value;
+                else
+                    throw new Exception("The power of an engine can not be negative or equal to 0.");
+            }
         }
 
         public float EngineVolume
         {
-            get { return Engine.Volume; }
-            set { Engine.Volume = value; }
+            get
+            {
+                if (Engine.Volume > 0)
+                    return Engine.Volume;
+                else
+                    throw new Exception("The volume of an engine can not be negative or equal to 0.");
+            }
+
+            set
+            {
+                if (Engine.Volume > 0)
+                    Engine.Volume = value;
+                else
+                    throw new Exception("The volume of an engine can not be negative or equal to 0.");
+            }
         }
 
         public string EngineType
@@ -33,42 +59,120 @@ namespace CarPark
 
         public int EngineSerialNumber
         {
-            get { return Engine.EngineSerialNumber; }
-            set { Engine.EngineSerialNumber = value; }
+            get
+            {
+                if (Engine.EngineSerialNumber.ToString().Length == 9)
+                    return Engine.EngineSerialNumber;
+                else
+                    throw new Exception("The serial number of an engine should be 9 digits long.");
+            }
+
+            set
+            {
+                if (Engine.EngineSerialNumber.ToString().Length == 9)
+                    Engine.EngineSerialNumber = value;
+                else
+                    throw new Exception("The serial number of an engine should be 9 digits long.");
+            }
         }
 
         // Properties of Chassis class
 
         public int WheelsNumber
         {
-            get { return Chassis.WheelsNumber; }
-            set { Chassis.WheelsNumber = value; }
+            get
+            {
+                if (Chassis.WheelsNumber > 0 && Chassis.WheelsNumber <= 12)
+                    return Chassis.WheelsNumber;
+                else
+                    throw new Exception("A vehicle must have a wheel count greater than 0 and less than or equal to 12.");
+            }
+
+            set
+            {
+                if (Chassis.WheelsNumber >= 0 && Chassis.WheelsNumber <= 12)
+                    Chassis.WheelsNumber = value;
+                else
+                    throw new Exception("A vehicle must have a wheel count greater than 0 and less than or equal to 12.");
+            }
         }
 
         public int PermissibleLoad
         {
-            get { return Chassis.PermissibleLoad; }
-            set { Chassis.PermissibleLoad = value; }
+            get
+            {
+                if (Chassis.PermissibleLoad > 0)
+                    return Chassis.PermissibleLoad;
+                else
+                    throw new Exception("Permissible load of a vehicle must be a possitive number.");
+            }
+
+            set
+            {
+                if (Chassis.PermissibleLoad > 0)
+                    Chassis.PermissibleLoad = value;
+                else
+                    throw new Exception("Permissible load of a vehicle must be a possitive number.");
+            }
         }
 
         public int ChassisNumber
         {
-            get { return Chassis.ChassisNumber; }
-            set { Chassis.ChassisNumber = value; }
+            get
+            {
+                if (Chassis.ChassisNumber.ToString().Length == 8)
+                    return Chassis.ChassisNumber;
+                else
+                    throw new Exception("The serial number of a chassis should be 8 digits long.");
+            }
+
+            set
+            {
+                if (Chassis.ChassisNumber.ToString().Length == 8)
+                    Chassis.ChassisNumber = value;
+                else
+                    throw new Exception("The serial number of an chassis should be 8 digits long.");
+            }
         }
 
         // Properties of Transmission class
 
         public int NumberOfGears
         {
-            get { return Transmission.NumberOfGears; }
-            set { Transmission.NumberOfGears = value; }
+            get
+            {
+                if (Transmission.NumberOfGears > 0)
+                    return Transmission.NumberOfGears;
+                else
+                    throw new Exception("Number of gears can not be negative.");
+            }
+
+            set
+            {
+                if (Transmission.NumberOfGears > 0)
+                    Transmission.NumberOfGears = value;
+                else
+                    throw new Exception("Number of gears can not be negative.");
+            }
         }
 
         public string Manufacturer
         {
-            get { return Transmission.Manufacturer; }
-            set { Transmission.Manufacturer = value; }
+            get
+            {
+                if (string.IsNullOrEmpty(Transmission.Manufacturer))
+                    throw new Exception("The transmission manufacturer can not be empty");
+                else
+                    return Transmission.Manufacturer;
+            }
+
+            set
+            {
+                if (string.IsNullOrEmpty(Transmission.Manufacturer))
+                    throw new Exception("The transmission manufacturer can not be empty");
+                else
+                    Transmission.Manufacturer = value;
+            }
         }
 
         public string TransmissionType
@@ -79,8 +183,21 @@ namespace CarPark
 
         public int TransmissionSerialNumber
         {
-            get { return Transmission.TransmissionSerialNumber; }
-            set { Transmission.TransmissionSerialNumber = value; }
+            get
+            {
+                if (Transmission.TransmissionSerialNumber.ToString().Length == 7)
+                    return Transmission.TransmissionSerialNumber;
+                else
+                    throw new Exception("The serial number of a transmission should be 7 digits long.");
+            }
+
+            set
+            {
+                if (Transmission.TransmissionSerialNumber.ToString().Length == 7)
+                    Transmission.TransmissionSerialNumber = value;
+                else
+                    throw new Exception("The serial number of a transmission should be 7 digits long.");
+            }
         }
     }
 }
